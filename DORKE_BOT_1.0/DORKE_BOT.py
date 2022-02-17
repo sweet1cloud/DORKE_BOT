ff699fde2f0671cbdf40338ede716b495d4f2180
Sweet_Cloud <45199326+sweet1cloud@users.noreply.github.com>
2022-02-17 14:01:28 +0900

    Update DORKE_BOT.py

--- DORKE_BOT_1.0/DORKE_BOT.py
+++ DORKE_BOT_1.0/DORKE_BOT.py
@@ -6,13 +6,13 @@
 #도움을 주신 분: 기우
 #2020-06-21 대규모 업데이트
 #2020-06-22 추가 업데이트
-#아래 내용부터 봇의 코드입니다.
 #도르크봇 2.0도 추후 개발 예정입니다.
+#2021년 8월 28일 discord.py의 개발 및 서비스 종료
+#2022-02-17 discord.py → nextcord (https://github.com/nextcord/nextcord)
+#아래 내용부터 봇의 코드입니다.
 #----------------------------
 import discord, asyncio, random, datetime, time
-from discord.ext import commands
-from discord.ext.commands import Bot
-from discord.utils import get
+from nextcord.ext import commands
 
 uid = discord_user_id #'discord_user_id' 부분에다가 본인의 디스코드 ID를 입력하세요.
 token = "bot_token" #'bot_token' 부분에다가 봇 토큰을 입력하세요.
@@ -20,7 +20,7 @@ app = commands.Bot(command_prefix='d#')
 
 @app.event
 async def on_ready():
-    print("Fix. 191210 | Last 20210206 ver.")
+    print("Fix. 191210 | Last 20220217 ver.")
     print("")
     print("■■■■■■■■■■■■■■■[ I  N  F  O ]■■■■■■■■■■■■■■■")
     print("")
@@ -35,15 +35,15 @@ async def on_ready():
     print("이 메시지가 뜬다면 봇 디버깅에 성공한 것입니다.")
     game = discord.Game("[d#도움말]도르크봇을 불러보세요!")
     await app.change_presence(status=discord.Status.online, activity=game)
-    channel = app.get_channel(709228507540095028) #도르크 프로젝트
+    channel = app.get_channel(709228507540095028) #도르크 프로젝트 봇-로그 채널 ID
     await channel.send('봇켜짐')
 
 @app.command(pass_context=True)
 async def 도움말(ctx):
     embed = discord.Embed(
             title="도움말",
-            description="> 도르크봇 1.0 버전을 이용해주셔서 감사합니다!\n> 봇 공식 페이지에서 봇 명령어를 확인하실 수 있습니다!\n ``봇 공식 페이지:`` [도르크봇 깃헙](https://github.com/sweet1cloud/DORKE_BOT)\n ```Bot Running Information \n 현재 돌아가고 있는곳 : 개발자 컴퓨터에서 구동 중입니다!(24시간 X) \n 프로그래밍 언어 : Python 3.7.0 32-bit (With VSC)``` \n```공지 시스템: DPNK\n봇 소스: The Circles! ProJect, Aria_Public ProJect\n(라이센스 위반 없이 사용중.)```\n ```개발자 연락처: 디스코드: [프로필 보기](https://discord.com/users/284143521886109697), \n이메일: idoyun027@gmail.com```\n```help - DM으로 전송\n도움 - 서버 채팅 채널에 전송```\n``봇 참고 레포지트리:`` [the circle prj](https://github.com/Ryzen72700/thecircle_prj/blob/master/tc_github.py)\n ``봇 참고 레포지트리:`` [Aria prj](https://github.com/Ryzen72700/DISCORDPY_Aria/blob/master/Aria_Public.py)\n``개발자 유튜브``: [youtube](https://www.youtube.com/channel/UC1v2JDiftMw7epyndnVA_Bg)",
-            timestamp=datetime.datetime.utcnow(),
+            description="> 도르크봇 1.0 버전을 이용해주셔서 감사합니다!\n> 봇 공식 페이지에서 봇 명령어를 확인하실 수 있습니다!\n ```Bot Running Information \n 현재 돌아가고 있는 곳 : 개발자 컴퓨터에서 구동 중입니다! (24시간 X)\n 프로그래밍 언어 : Python 3.8.8 64-bit (With VSCode, nextcord)```\n```공지 시스템: DPNK\n봇 소스: The Circles! ProJect, Aria_Public ProJect\n(라이선스 위반 없이 사용 중.)```\n```d#도움말 - 현재 명령어\nd#ping - 클라이언트 지연 시간(핑) 확인\nd#invite - 봇 초대코드\nd#랜덤로고 - 랜덤으로 로고를 보여줍니다.\nd#반응 - 여러가지 감정 반응이 있어요!```\n``개발자 디스코드 프로필 보기``:[링크](https://discord.com/users/284143521886109697) \n``봇 공식 페이지``: [도르크봇 깃헙](https://github.com/sweet1cloud/DORKE_BOT) \n``봇 참고 레포지트리``: [The Circles! Project](https://github.com/Ryzen72700/thecircle_prj/blob/master/tc_github.py), [Aria_Public ProJect](https://github.com/Ryzen72700/DISCORDPY_Aria/blob/master/Aria_Public.py)\n``개발자 유튜브``: [YouTube](https://www.youtube.com/channel/UC1v2JDiftMw7epyndnVA_Bg)\n``이메일``: idoyun027@gmail.com",
+            timestamp=datetime.datetime.now(),
             color=0x99EAF5
         )
     await ctx.channel.send(embed=embed)
@@ -158,16 +158,17 @@ async def 도배(ctx):
         )
     await ctx.send(embed=embed)
 
-@app.event
-async def on_message(message):
-    if message.content.startswith("d#정보"):
-        date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
-        embed = discord.Embed(color=0x00ff00)
-        embed.add_field(name="이름", value=message.author.name, inline=True)
-        embed.add_field(name="서버닉네임", value=message.author.display_name, inline=True)
-        embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일", inline=True)
-        embed.add_field(name="아이디", value=message.author.id, inline=True)
-        embed.set_thumbnail(url=message.author.avatar_url)
-        await message.channel.send(embed=embed)
+#임시 비활성화
+#@app.event
+#async def on_message(message):
+    #if message.content.startswith("d#정보"):
+        #date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
+        #embed = discord.Embed(color=0x00ff00)
+        #embed.add_field(name="이름", value=message.author.name, inline=True)
+        #embed.add_field(name="서버닉네임", value=message.author.display_name, inline=True)
+        #embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일", inline=True)
+        #embed.add_field(name="아이디", value=message.author.id, inline=True)
+        #embed.set_thumbnail(url=message.author.avatar_url)
+        #await message.channel.send(embed=embed)
 
 app.run(token)
